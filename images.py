09dfa78574049285950e648dbfb1d2b24e3f7df3
bengenseo <bengenseo@126.com>
2024-08-11 17:46:35 +0800

    Auto-commit: 2024-08-11 17:46:35

--- images.py
+++ images.py
@@ -1,5 +1,5 @@
 import base64
 
-with open("icon.ico", "rb") as icon_file:
+with open("logo.ico", "rb") as icon_file:
     encoded_string = base64.b64encode(icon_file.read()).decode('utf-8')
     print(encoded_string)
