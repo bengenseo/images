8e3c2a1fd637f8e22f79bc87b2303f78eaa6d42b
bengenseo <bengenseo@126.com>
2024-08-11 17:45:27 +0800

    Auto-commit: 2024-08-11 17:45:27

--- images.py
+++ images.py
@@ -1,6 +1,4 @@
-# 读取图片文件
-with open("logo.png", "rb") as image_file:
-    # 将图片转换为十六进制字符串
-    hex_string = image_file.read().hex()
-    logo="https://images.sjooo.sbs/logo.png"
-print(hex_string,logo)
+
+logo="logo.png"
+
+logo.read().hex()
